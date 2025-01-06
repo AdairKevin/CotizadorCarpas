7f47b769f103e8ad2725222a2866cd722fb4cf8c
Adair <quesuabe@gmail.com>
2025-01-06 16:55:05 -0600

    Primer Commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const form = document.querySelector('form');
+const totalElement = document.getElementById('total');
+const listaElement = document.getElementById('lista-productos');
+
+form.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+
+    const metros = parseInt(document.getElementById('metros').value) || 0;
+    const servicio = document.getElementById('servicio').value;
+    const mesas = parseInt(document.getElementById('mesas').value) || 0;
+    const sillas = parseInt(document.getElementById('sillas').value) || 0;
+    const decoracion = document.getElementById('decoracion').value;
+    const pasto = parseInt(document.getElementById('pasto').value) || 0;
+
+
+
+    //Definicion de los costos
+    const costos = {
+        normal: { metro: 240, mesa: 0 },
+        frances: { metro: 240, mesa: 80 },
+        imperial: { metro: 240, mesa: 450 }
+    };
+
+    const costosDecoraciones = {
+        normal: {metro: 50, completa:0 },
+        plafon: {metro: 0, completa: 3000},
+        flor: {metro: 0, completa: 1500}
+    }
+
+    const costoMetroPasto = 17;
+
+    let total = 0;
+    let mensajeError = '';
+
+    if (costos[servicio] && mesas <= metros) {
+        const sillasRequeridas = mesas * 10;  // Por cada mesa hay 10 sillas
+        if (sillas !== sillasRequeridas) {
+            mensajeError = `El número de sillas debe ser ${sillasRequeridas} para ${mesas} mesas.`;
+        } else {
+            total = (metros * costos[servicio].metro) + (mesas * costos[servicio].mesa);
+
+            if (costosDecoraciones[decoracion]) {
+                if (decoracion === 'normal') {
+                    total += metros * costosDecoraciones[decoracion].metro;  
+                } else {
+                    total += costosDecoraciones[decoracion].completa;
+                }
+            }
+
+            console.log(total);
+
+            if(pasto>0){
+                total += (pasto * 8) * costoMetroPasto;
+            }
+
+            console.log(total);
+        }
+    } else if (mesas > metros) {
+        mensajeError = `La cantidad de mesas seleccionadas no caben en ${metros} metros`;
+    } else {
+        mensajeError = "Ocurrió un error y no sabemos por qué, intentalo de nuevo";
+    }
+
+    listaElement.textContent = '';
+
+    const cotizacion = [
+        { label: `Metros de carpa: 8 x ${metros} `, value: metros },
+        { label: `Cantidad de mesas: ${mesas}`, value: mesas },
+        { label: `Cantidad de sillas: ${sillas} `, value: sillas },
+        { label: `Servicio: ${servicio}`, value: servicio },
+        { label: `Decoracion: ${decoracion} `, value: decoracion },
+        { label: `Metros de pasto: ${pasto} `, value: pasto },
+    ];
+    
+    cotizacion.forEach(item => {
+        if (item.value > 0 || item.value) {
+            listaElement.append(item.label, document.createElement('br'));
+        }
+    });
+
+    totalElement.textContent = mensajeError || `${total}`
+
+
+});
